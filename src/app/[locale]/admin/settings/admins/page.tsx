2ca75413fd63b05a5a500fcc084a4410afb6220b
Shadi-techs <shadyabdelsalam7@gmail.com>
2026-06-07 14:32:54 +0000

    fix: add notification_permissions in admin reset state

--- src/app/[locale]/admin/settings/admins/page.tsx
+++ src/app/[locale]/admin/settings/admins/page.tsx
@@ -184,7 +184,8 @@ export default function AdminManagementPage({ params }: { params: { locale: stri
         can_delete_properties: false,
         can_approve_listings: false,
         can_manage_bookings: false,
-        can_view_financials: false
+        can_view_financials: false,
+        notification_permissions: ['new_listing', 'moderation']
       })
 
     } catch (err) {
